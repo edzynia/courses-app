561e57eaa4d4c4d649129aed0686d4dda6e1ba9f
Eugenia Dzynia <eugenia.dzynia@edaider.com>
2023-06-23 20:39:50 +0200

    updates

--- keys/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://admin:admin@cluster0.jice2jb.mongodb.net/shop',
-  PORT: process.env.PORT || 3000,
-  SESSION_SECRET: 'some',
-  SENDGRID_API_KEY:
-    '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',
-  SEND_EMAIL: 'eugenia.dzynia@gmail.com',
-  BASE_URL: 'http://localhost:3000/',
-};
